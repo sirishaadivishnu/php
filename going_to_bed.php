04fa6a34a16e855d0bb85da2658455ec71b3a924
sirisha6409 <41979625+sirishaadivishnu@users.noreply.github.com>
2023-05-16 09:58:13 +0100

    finalized bed

--- going_to_bed.php
+++ going_to_bed.php
@@ -15,12 +15,8 @@ class StringUtils
 class Pajamas
 {
   private $owner, $fit, $color;
-
-  function __construct(
-    $owner = "unclaimed",
-    $fit = "fine",
-    $color = "white"
-  ) {
+ 
+  function __construct($owner = "unclaimed", $fit = "fine", $color = "white") {
     $this->owner = StringUtils::secondCase($owner);
     $this->fit = $fit;
     $this->color = $color;
@@ -28,16 +24,12 @@ class Pajamas
   public function describe() {
     return "$this->owner's $this->color pajamas fit $this->fit.";
   }
- $chicken_PJs = new Pajamas("CHICKEN", "just right", "purple");
-  echo $chicken_PJs->describe();
-
+ 
+ 
   public function setFit($new_fit) {
   $this->fit = $new_fit;
   }
-  echo "\n...they wash their PJs many times....";
-  $chicken_PJs->setFit("a little tight");
-  echo "\n";
-  echo $chicken_PJs->describe();
+ 
 }
 class ButtonablePajamas extends Pajamas
 {
@@ -45,9 +37,7 @@ class ButtonablePajamas extends Pajamas
   public function describe() {
   return parent::describe() . " They also have buttons which are currently $this->button_state.";
   }
-  $moose_PJs = new ButtonablePajamas("moose", "kind of loose", "red");
-  echo "\n";
-  echo $moose_PJs->describe();
+ 
   public function toggleButtons() {
     if ($this->button_state === "unbuttoned") {
       $this->button_state = "buttoned";
@@ -55,7 +45,18 @@ class ButtonablePajamas extends Pajamas
       $this->button_state = "unbuttoned";
     }
   }
-  $moose_PJs->toggleButtons();
-  echo "\n";
-  echo $moose_PJs->describe();
 }
+ 
+$chicken_PJs = new Pajamas("CHICKEN", "just right", "purple");
+echo $chicken_PJs->describe();
+echo "\n...they wash their PJs many times....";
+$chicken_PJs->setFit("a little tight");
+echo "\n";
+echo $chicken_PJs->describe();
+ 
+$moose_PJs = new ButtonablePajamas("moose", "kind of loose", "red");
+echo "\n";
+echo $moose_PJs->describe();
+$moose_PJs->toggleButtons();
+echo "\n";
+echo $moose_PJs->describe();
